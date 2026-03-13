46e20af23f31808b42a4d667c94daa64d8389ec3
Andrea Paris <andreaparis.es@gmail.com>
2026-03-13 13:48:03 +0000

    Create etl_pipeline.py

---
+++ etl_pipeline.py
@@ -0,0 +1,77 @@
+import glob 
+import pandas as pd 
+import xml.etree.ElementTree as ET 
+from datetime import datetime 
+
+# Global variables
+log_file = "log_file.txt" 
+target_file = "transformed_data.csv" 
+
+# EXTRACT
+# Defining extraction functions
+def extract_from_csv(file_to_process):
+    dataframe = pd.read_csv(file_to_process)
+    return dataframe
+
+def extract_from_json(file_to_process):
+    dataframe = pd.read_json(file_to_process)
+    return dataframe
+
+def extract_from_xml(file_to_process):
+    dataframe = pd.DataFrame(columns=["name", "height", "weight"])
+    tree = ET.parse(file_to_process)
+    root = tree.getroot()
+    for person in root:
+        name = person.find("name").text
+        height = float(person.find("height").text)
+        weight = float(person.find("weight").text)
+        dataframe = pd.concat([dataframe, pd.DataFrame([{"name":name, "height":height, "weight":weight}])], ignore_index=True)
+    return dataframe
+
+# Function to identify which function to call depending on filetype of the file
+def extract():
+    extracted_data = pd.DataFrame(columns=["name", "height", "weight"])
+    for csvfile in glob.glob("*.csv"):
+        if csvfile != target_file:
+            extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_csv(csvfile))], ignore_index=True)
+    for jsonfile in glob.glob("*.json"):
+        extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_json(jsonfile))], ignore_index=True)
+    for xmlfile in glob.glob("*.xml"):
+        extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_xml(xmlfile))], ignore_index=True)
+    return extracted_data
+
+# TRANSFORM
+def transform(data):
+    '''Convert inches to meters and round off to two decimals. 1 inch is 0.0254 metres.'''
+    data["height"] = round(data.height * 0.0254, 2)
+    ''' Convert pounds to kg and round off to two decimals. 1 pound is 0.45359237 kg.'''
+    data["weight"] = round(data.weight * 0.45359237, 2)
+    return data
+
+# LOADING AND LOGGING
+def load_data(target_file, transformed_data):
+    transformed_data.to_csv(target_file, index=False)
+
+def log_progress(message):
+    timestamp_format = '%Y-%h-%d-%H:%M:%S'
+    now = datetime.now()
+    timestamp = now.strftime(timestamp_format)
+    with open(log_file, "a") as f:
+        f.write(timestamp + ',' + message + '\n')
+
+# Main pipeline execution
+def main():
+    log_progress("ETL Job Started")
+
+    extracted_data = extract()
+    log_progress("Data extraction completed")
+
+    transformed_data = transform(extracted_data)
+    log_progress("Data transformation completed")
+
+    load_data(target_file, transformed_data)
+    log_progress("Data loaded successfully")
+
+
+if __name__ == "__main__":
+    main()
